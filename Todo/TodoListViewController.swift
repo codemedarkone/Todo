d94f39febf94232157d5fe28210531f720cd3c3c
Chris <codemedarkone@gmail.com>
2018-09-20 18:35:55 -0600

    added user defaults

--- Todo/TodoListViewController.swift
+++ Todo/TodoListViewController.swift
@@ -12,11 +12,17 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = ["find mike", "buy eggs", "Morgan Freeman"]
     
+    let defaults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        guard let items = defaults.array(forKey: "TodoListArray") as? [String] else { return }
+        itemArray = items
+//        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+//            itemArray = items
+//        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,6 +64,8 @@ class TodoListViewController: UITableViewController {
             print(textField.text)
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
